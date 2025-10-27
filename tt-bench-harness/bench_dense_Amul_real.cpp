d21975a8b23599efe5057f8f8b415cc8b6ecd3fb
Adrian Schmitz <36474667+Atrisan@users.noreply.github.com>
2025-10-27 14:23:23 +0100

    add consistent element type

--- tt-bench-harness/bench_dense_Amul_real.cpp
+++ tt-bench-harness/bench_dense_Amul_real.cpp
@@ -220,6 +220,7 @@ int main(int argc, char* argv[]) {
         .line_end = 235,
         .column_begin = 0,
         .column_end = 0,
+        .element_type = "map",
         .target_type = "TENSTORRENT",
         .region_uuid = "tt_launch_dense_matMul"
     };
