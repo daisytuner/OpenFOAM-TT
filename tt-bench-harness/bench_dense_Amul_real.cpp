1c66c1b23731e58b9e2cb36c92cbd2f9e004f138
Adrian Schmitz <36474667+Atrisan@users.noreply.github.com>
2025-10-27 17:29:21 +0100

    add fake sdfg data

--- tt-bench-harness/bench_dense_Amul_real.cpp
+++ tt-bench-harness/bench_dense_Amul_real.cpp
@@ -141,7 +141,11 @@ int main(int argc, char* argv[]) {
 
 
      #ifdef ENABLE_DAISY_RTL
-    __daisy_metadata_t metadata1 = {
+    __daisy_metadata_t metadata1 = {.file_name = "instrumentation_test.c",
+        .sdfg_name = "__daisy_instrumentation_test_0",
+        .sdfg_file = "/tmp/DOCC/0000-0000/123456789/sdfg_0.json",
+        .element_id = 10,
+        .loopnest_index = -1,
         .file_name = "bench_dense_Amul_real.cpp",
         .function_name = "copy_ldu_to_dense",
         .line_begin = 154,
@@ -171,6 +175,10 @@ int main(int argc, char* argv[]) {
 
     #ifdef ENABLE_DAISY_RTL
     __daisy_metadata_t metadata2 = {
+        .sdfg_name = "__daisy_instrumentation_test_0",
+        .sdfg_file = "/tmp/DOCC/0000-0000/123456789/sdfg_0.json",
+        .element_id = 10,
+        .loopnest_index = -1,
         .file_name = "bench_dense_Amul_real.cpp",
         .function_name = "copy_scalarfield",
         .line_begin = 178,
@@ -214,6 +222,10 @@ int main(int argc, char* argv[]) {
 
     #ifdef ENABLE_DAISY_RTL
     __daisy_metadata_t metadata3 = {
+        .sdfg_name = "__daisy_instrumentation_test_0",
+        .sdfg_file = "/tmp/DOCC/0000-0000/123456789/sdfg_0.json",
+        .element_id = 10,
+        .loopnest_index = 0,
         .file_name = "bench_ldu_Amul_real.cpp",
         .function_name = "tt_launch_dense_matMul",
         .line_begin = 218,
@@ -265,6 +277,10 @@ int main(int argc, char* argv[]) {
 
     #ifdef ENABLE_DAISY_RTL
     __daisy_metadata_t metadata4 = {
+        .sdfg_name = "__daisy_instrumentation_test_0",
+        .sdfg_file = "/tmp/DOCC/0000-0000/123456789/sdfg_0.json",
+        .element_id = 10,
+        .loopnest_index = 0,
         .file_name = "bench_dense_Amul_real.cpp",
         .function_name = "copy_scalarField_from_device_dense_mat",
         .line_begin = 267,
