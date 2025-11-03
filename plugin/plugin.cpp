f53b82ac803177a44bbd06ee25fed496085b5b70
Moritz Timmer <25349452+Moehre2@users.noreply.github.com>
2025-11-03 17:52:48 +0100

    Replace Amul with an empty SDFG

--- plugin/plugin.cpp
+++ plugin/plugin.cpp
@@ -1,29 +1,42 @@
+#include <memory>
+#include <sdfg/builder/structured_sdfg_builder.h>
 #include <sdfg/codegen/dispatchers/node_dispatcher_registry.h>
+#include <sdfg/function.h>
 #include <sdfg/plugins/plugins.h>
 #include <sdfg/serializer/json_serializer.h>
+#include <sdfg/types/pointer.h>
+#include <sdfg/types/scalar.h>
+#include <sdfg/types/type.h>
+#include <string>
 
-#include "simple_node.h"
+#include "sdfg/structured_sdfg.h"
+
+std::unique_ptr<sdfg::StructuredSDFG> create_Amul() {
+    sdfg::builder::StructuredSDFGBuilder builder("_ZNK4Foam9lduMatrix4AmulERNS_5FieldIfEERKNS_3tmpIS2_EERKNS_10FieldFieldIS1_fEERKNS_8UPtrListIKNS_17lduInterfaceFieldEEEh", sdfg::FunctionType_CPU);
+
+    sdfg::types::Scalar int8_type(sdfg::types::PrimitiveType::Int8);
+    sdfg::types::Pointer void_pointer;
+    builder.add_container("_0", void_pointer, true);
+    builder.add_container("_1", void_pointer, true);
+    builder.add_container("_2", void_pointer, true);
+    builder.add_container("_3", void_pointer, true);
+    builder.add_container("_4", void_pointer, true);
+    builder.add_container("_5", int8_type, true);
+
+    return builder.move();
+}
 
 extern "C" sdfg::plugins::Plugin register_docc_plugin() {
     return {
         .name = "OpenFOAM_DOCCPlugin",
         .version = "0.0.1",
         .description = "A plugin that provides OpenFOAM kernels for docc",
-        .register_plugin_callback = []() {
-            sdfg::serializer::LibraryNodeSerializerRegistry::instance()
-                .register_library_node_serializer(simple::LibraryNodeType_Simple.value(), []() {
-                    return std::make_unique<simple::SimpleNodeSerializer>();
-                });
-
-            sdfg::codegen::LibraryNodeDispatcherRegistry::instance().register_library_node_dispatcher(
-                simple::LibraryNodeType_Simple.value() + "::" + sdfg::data_flow::ImplementationType_NONE.value(),
-                [](sdfg::codegen::LanguageExtension& language_extension,
-                const sdfg::Function& function,
-                const sdfg::data_flow::DataFlowGraph& data_flow_graph,
-                const sdfg::data_flow::LibraryNode& node) {
-                    return std::make_unique<simple::SimpleDispatcher>(language_extension, function, data_flow_graph, node);
-                }
-            );
+        .register_plugin_callback = []() {},
+        .sdfg_lookup = [](std::string name) {
+            if (name == "_ZNK4Foam9lduMatrix4AmulERNS_5FieldIfEERKNS_3tmpIS2_EERKNS_10FieldFieldIS1_fEERKNS_8UPtrListIKNS_17lduInterfaceFieldEEEh") {
+                return create_Amul();
+            }
+            return std::unique_ptr<sdfg::StructuredSDFG>(nullptr);
         }
     };
 }
