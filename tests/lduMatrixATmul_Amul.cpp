79f4decae5ef22b5988c6ad2e697000257c4f36b
Moritz Timmer <25349452+Moehre2@users.noreply.github.com>
2025-11-24 13:57:17 +0100

    Fix test cases
    Call ldu_tt_meta_map.clear() to ensure that the buffers are destroyed before the device is closed in the test cases.

--- tests/lduMatrixATmul_Amul.cpp
+++ tests/lduMatrixATmul_Amul.cpp
@@ -1,6 +1,7 @@
 #include "SquareMatrix.H"
 #include "lduMatrix.H"
 #include "lduPrimitiveMesh.H"
+#include "ldu_meta_cache.hpp"
 
 __attribute__((noinline))
 void kernel(Foam::label& nCells, Foam::lduMatrix& matrix, Foam::scalarField& vec, Foam::FieldField<Foam::Field, Foam::scalar>& interfaceBouCoeffs, Foam::lduInterfaceFieldPtrsList& interfaces, Foam::direction& cmpt, Foam::scalarField& result1, Foam::scalarField& result2) {
@@ -86,6 +87,8 @@ int main()
             return 1;
         }
     }
+
+    tt::daisy::foam::ldu_tt_meta_map.clear();
 
     return 0;
 }
